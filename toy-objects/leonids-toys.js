698de4f31b6fab71b45a2cb76f39106ff3e28e03
warrior14 <ironaddicts14@gmail.com>
2021-05-27 10:24:55 -0500

    my first version of leonids-toys that contains the toys array with 3 toy objects

---
+++ toy-objects/leonids-toys.js
@@ -0,0 +1,25 @@
+const toys = [
+    {
+        id: 1,
+        name: "Hot Wheels",
+        inventory: 14,
+        isCheap: true,
+        countryOfOrigin: "China"
+    },    
+    {
+        id: 2,
+        name: "Legos",
+        inventory: 7,
+        isCheap: false,
+        countryOfOrigin: "Usa"
+    },
+    {
+        id: 3,
+        name: "Frisbee",
+        inventory: 21,
+        isCheap: true,
+        countryOfOrigin: "India"
+    }
+];
+
+console.log(toys);
